9d8a7d1260bc8028d1bd38734b7e4171af45f96a
Nox Dineen <noxdineen@gmail.com>
2012-09-28 10:51:13 -0400

    Added db connection and config globals. DB connection not up and running yet, committing to share files in current state.

---
+++ freshmeet.py
@@ -0,0 +1,29 @@
+# import all the things!
+from __future__ import with_statement
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+        abort, render_template, flash
+from contextlib import closing
+
+# config ahoy
+DATABASE = '/db/freshmeet.db'
+DEBUG = True
+SECRET_KEY = 'supersecret'
+USERNAME = 'admin'
+PASSWORD = 'password'
+
+# make rocket ship go
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+if __name__ == '__main__':
+    app.run()
