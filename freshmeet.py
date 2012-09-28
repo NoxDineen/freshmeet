1065793134d79fec17d8615b04fd0a8a9b257ea0
AntonNguyen <afnguyen85@gmail.com>
2012-09-28 13:28:04 -0400

    Added the home route and initialized the db

--- freshmeet.py
+++ freshmeet.py
@@ -32,10 +32,16 @@ def before_request():
 def teardown_request():
     d.db.close()
 
+@app.route('/')
+def show_schedule():
+    cur = g.db.execute('select room_id, name, capacity, type, num_whiteboards from rooms order by type, room_id')
+    rooms = [dict(id=row[0], name=row[1], capacity=row[2], type=row[3], num_whiteboards=row[4]) for row in cur.fetchall()]
+    return render_template('index.html', rooms = rooms)
+
 def list_reservations(day):
     day_reservations = g.db.execute(
         '''SELECT room_id, start_time, end_time, host, num_attendees, description
-        FROM reservations 
+        FROM reservations
         WHERE date(start_time) = day OR date(end_time) = day''')
     reservations = [dict(
             room=reservation[0],
@@ -50,7 +56,7 @@ def list_reservations(day):
 @app.route('/reservations', methods=['POST'])
 def add_reservation():
     g.db.execute('INSERT INTO reservations (reservation_id, room_id, start_time, end_time, host, num_attendees, description) values (?, ?)',
-                 [request.form['reservation_id'], request.form['room_id'], request.form['start_time'], request.form['end_time'], 
+                 [request.form['reservation_id'], request.form['room_id'], request.form['start_time'], request.form['end_time'],
                  request.form['host'], request.form['num_attendees'], request.form['description']])
     g.db.commit()
     flash('Your room reservation has been made.')
@@ -58,3 +64,4 @@ def add_reservation():
 
 if __name__ == '__main__':
     app.run()
+    init_db()
